a1f3b10a70c7df2f20b1f382904bd1585639fa10
Your Name <you@example.com>
2026-01-18 15:19:08 +0100

    Add slug to login page

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,32 +1,30 @@
-const appTitle = process.env.APP_TITLE || "App"
+const appTitle = process.env.APP_TITLE || "App";
 
 export default {
-    authGet: (req, res) => {
-        res.render('auth/login', {
-            appTitle: appTitle,
-            messages: req.session.messages,
-            title: "Admin"
-        })
-    },
-    registerGet: (req, res) => {
-        res.render('auth/register', { appTitle: appTitle, title: "Register" })
-    },
-    registerPost: async (req, res, next) => {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        userRepository.insertUser(
-            req.body.username,
-            hashedPassword,
-            true
-        )
-            .then(() => res.redirect('/login'))
-            .catch(err => next(err))
-    },
-    logout: (req, res, next) => {
-        req.logout((err) => {
-            if (err) {
-                return next(err);
-            }
-            res.redirect("/");
-        });
-    }
-}
+  authGet: (req, res) => {
+    res.render("auth/login", {
+      appTitle: appTitle,
+      messages: req.session.messages,
+      title: "Admin",
+      slug: "login",
+    });
+  },
+  registerGet: (req, res) => {
+    res.render("auth/register", { appTitle: appTitle, title: "Register" });
+  },
+  registerPost: async (req, res, next) => {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    userRepository
+      .insertUser(req.body.username, hashedPassword, true)
+      .then(() => res.redirect("/login"))
+      .catch((err) => next(err));
+  },
+  logout: (req, res, next) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  },
+};
