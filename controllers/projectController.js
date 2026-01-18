f6d7fb24ebca6b6b785792d95d2692e8585287e1
Your Name <you@example.com>
2026-01-18 14:54:24 +0100

    Fix slug issue

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,169 +1,184 @@
-import { body, validationResult } from 'express-validator'
-import projectRepository from '../repositories/projectRepository.js'
+import { body, validationResult } from "express-validator";
+import projectRepository from "../repositories/projectRepository.js";
 
-const lengthErr = " doit faire entre 3 et 15 charactères"
+const lengthErr = " doit faire entre 3 et 15 charactères";
 
 const validateProject = [
-    body('name').trim()
-        .isLength({ min: 3, max: 15 }).withMessage("Le nom " + lengthErr),
-    body('description').trim()
-        .isLength({ min: 3, max: 15 }).withMessage("La description " + lengthErr),
-    body('github_repo').isURL().withMessage('L\'url n\'est pas valide')
+  body("name")
+    .trim()
+    .isLength({ min: 3, max: 15 })
+    .withMessage("Le nom " + lengthErr),
+  body("description")
+    .trim()
+    .isLength({ min: 3, max: 15 })
+    .withMessage("La description " + lengthErr),
+  body("github_repo").isURL().withMessage("L'url n'est pas valide"),
+];
 
-]
-
-const appTitle = process.env.APP_TITLE
+const appTitle = process.env.APP_TITLE;
 /**
- * 
+ *
  */
 export default {
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    index: async (req, res) => {
-        const currentPage = req.query.p || 1
-        const count = await projectRepository.count()
-        const pages = Math.ceil(count / 8)
-        const offset = (currentPage - 1) * 8
-        projectRepository.getAllProjects(offset)
-            .then(projects => {
-                res.render('projects/index', {
-                    appTitle: appTitle,
-                    title: "Liste des projets",
-                    projects: projects,
-                    currentPage: currentPage,
-                    pages: pages,
-                    slug: "projects"
-                })
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    },
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  index: async (req, res) => {
+    const currentPage = req.query.p || 1;
+    const count = await projectRepository.count();
+    const pages = Math.ceil(count / 8);
+    const offset = (currentPage - 1) * 8;
+    projectRepository
+      .getAllProjects(offset)
+      .then((projects) => {
+        res.render("projects/index", {
+          appTitle: appTitle,
+          title: "Liste des projets",
+          projects: projects,
+          currentPage: currentPage,
+          pages: pages,
+          slug: "projects",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
 
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    create: (req, res) => {
-        res.render('projects/create', {
-            appTitle: appTitle,
-            title: "Ajouter un projet"
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  create: (req, res) => {
+    res.render("projects/create", {
+      appTitle: appTitle,
+      title: "Ajouter un projet",
+      slug: "projects",
+    });
+  },
+
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  details: (req, res) => {
+    projectRepository
+      .getProjectById(req.params.id)
+      .then((project) => {
+        res.render("projects/details", {
+          appTitle: appTitle,
+          title: `Détails du projet ${project.name}`,
+          project: project,
+          slug: "projects",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  store: [
+    validateProject,
+    (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).render("projects/create", {
+          appTitle: appTitle,
+          title: "Ajouter un projet",
+          errors: errors.array(),
+          slug: "projects",
+        });
+      }
+      projectRepository
+        .createProject(req.body)
+        .then(() => {
+          res.redirect("/projects");
         })
+        .catch((err) => {
+          console.log(err);
+        });
     },
+  ],
 
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    details: (req, res) => {
-        projectRepository.getProjectById(req.params.id)
-            .then(project => {
-                res.render('projects/details', {
-                    appTitle: appTitle,
-                    title: `Détails du projet ${project.name}`,
-                    project: project
-                })
-            })
-            .catch(err => {
-                console.log(err);
-            })
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  update: [
+    validateProject,
+    (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return projectRepository
+          .getProjectById(req.params.id)
+          .then((project) => {
+            res.status(4000).render("projects/edit", {
+              appTitle: appTitle,
+              title: "Editer le projet",
+              project: project,
+              errors: errors,
+              slug: "projects",
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+      projectRepository
+        .updateProject(req.params.id, req.body)
+        .then(() => {
+          res.redirect("/projects");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
+  ],
 
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    store: [
-        validateProject,
-        (req, res) => {
-            const errors = validationResult(req)
-            if (!errors.isEmpty()) {
-                return res.status(400).render('projects/create', {
-                    appTitle: appTitle,
-                    title: "Ajouter un projet",
-                    errors: errors.array()
-                })
-            }
-            projectRepository.createProject(req.body)
-                .then(() => {
-                    res.redirect("/projects")
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-        }
-    ],
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  edit: (req, res) => {
+    projectRepository
+      .getProjectById(req.params.id)
+      .then((project) => {
+        res.render("projects/edit", {
+          appTitle: appTitle,
+          title: "Editer le projet",
+          project: project,
+          slug: "projects",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
 
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    update: [
-        validateProject,
-        (req, res) => {
-            const errors = validationResult(req)
-            if (!errors.isEmpty()) {
-                return projectRepository.getProjectById(req.params.id)
-                    .then(project => {
-                        res.status(4000).render('projects/edit', {
-                            appTitle: appTitle,
-                            title: "Editer le projet",
-                            project: project,
-                            errors: errors
-                        })
-                    })
-                    .catch(err => {
-                        console.log(err);
-                    })
-            }
-            projectRepository.updateProject(req.params.id, req.body)
-                .then(() => {
-                    res.redirect('/projects')
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-        }
-    ],
-
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    edit: (req, res) => {
-        projectRepository.getProjectById(req.params.id)
-            .then(project => {
-                res.render('projects/edit', {
-                    appTitle: appTitle,
-                    title: "Editer le projet",
-                    project: project
-                })
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    },
-
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    delete: (req, res) => {
-        projectRepository.deleteProject(req.params.id)
-            .then(() => {
-                res.redirect('/projects')
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }
-}
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  delete: (req, res) => {
+    projectRepository
+      .deleteProject(req.params.id)
+      .then(() => {
+        res.redirect("/projects");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
--- controllers/snippetController.js
+++ controllers/snippetController.js
@@ -1,154 +1,169 @@
-import snippetRepository from '../repositories/snippetRepository.js'
-import languageRepository from '../repositories/languageRepository.js'
-import tagRepository from '../repositories/tagRepository.js'
-import projectRepository from '../repositories/projectRepository.js'
-import { body, validationResult } from 'express-validator'
+import snippetRepository from "../repositories/snippetRepository.js";
+import languageRepository from "../repositories/languageRepository.js";
+import tagRepository from "../repositories/tagRepository.js";
+import projectRepository from "../repositories/projectRepository.js";
+import { body, validationResult } from "express-validator";
 
-const appTitle = process.env.APP_TITLE
+const appTitle = process.env.APP_TITLE;
 
-const lengthMinErr = "doit contenir au moins 2 caractères."
-const lengthMinDescErr = "doit contenir au moins 8 caractères."
-const lengthMaxErr = "ne peut pas dépasser 10 caractères."
+const lengthMinErr = "doit contenir au moins 2 caractères.";
+const lengthMinDescErr = "doit contenir au moins 8 caractères.";
+const lengthMaxErr = "ne peut pas dépasser 10 caractères.";
 
 const validateSnippet = [
-    body('title').trim()
-        .isLength({ min: 2 }).withMessage('Nom ' + lengthMinErr)
-        .isLength({ max: 10 }).withMessage('Nom ' + lengthMaxErr),
-    body('description').trim()
-        .isLength({ min: 8 }).withMessage('Description ' + lengthMinDescErr)
-]
+  body("title")
+    .trim()
+    .isLength({ min: 2 })
+    .withMessage("Nom " + lengthMinErr)
+    .isLength({ max: 10 })
+    .withMessage("Nom " + lengthMaxErr),
+  body("description")
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage("Description " + lengthMinDescErr),
+];
 
 export default {
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    index: async (req, res) => {
-        const { count } = await snippetRepository.count()
-        const currentPage = Number(req.query.p) || 1
-        const pages = Math.ceil(count / 8)
-        const offset = (currentPage - 1) * 8
-        res.render('snippets/index', {
-            appTitle: appTitle,
-            title: "Liste des snippets",
-            snippets: await snippetRepository
-                .getPaginatedSnippetsWithLanguageAndProjectsAndTags(offset),
-            currentPage: currentPage,
-            slug: "snippets",
-            pages: pages
-        })
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  index: async (req, res) => {
+    const { count } = await snippetRepository.count();
+    const currentPage = Number(req.query.p) || 1;
+    const pages = Math.ceil(count / 8);
+    const offset = (currentPage - 1) * 8;
+    res.render("snippets/index", {
+      appTitle: appTitle,
+      title: "Liste des snippets",
+      snippets:
+        await snippetRepository.getPaginatedSnippetsWithLanguageAndProjectsAndTags(
+          offset,
+        ),
+      currentPage: currentPage,
+      slug: "snippets",
+      pages: pages,
+    });
+  },
+
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  details: async (req, res) => {
+    res.render("snippets/details", {
+      appTitle: appTitle,
+      title: "Details",
+      snippet:
+        await snippetRepository.getSnippetByIdWithLanguageAndProjectsAndTags(
+          req.params.id,
+        ),
+      slug: "snippets",
+    });
+  },
+
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  edit: async (req, res) => {
+    res.render("snippets/edit", {
+      appTitle: appTitle,
+      title: "Edit",
+      snippet:
+        await snippetRepository.getSnippetByIdWithLanguageAndProjectsAndTags(
+          req.params.id,
+        ),
+      languages: await languageRepository.getAllLanguages(),
+      tags: await tagRepository.getAllTags(),
+      projects: await projectRepository.getAllProjects(),
+    });
+  },
+
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  update: [
+    validateSnippet,
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).render("snippets/edit", {
+          appTitle: appTitle,
+          title: "Edit",
+          snippet:
+            await snippetRepository.getSnippetByIdWithLanguageAndProjectsAndTags(
+              req.params.id,
+            ),
+          languages: await languageRepository.getAllLanguages(),
+          tags: await tagRepository.getAllTags(),
+          projects: await projectRepository.getAllProjects(),
+          errors: errors.array(),
+        });
+      }
+      snippetRepository
+        .updateSnippetById(req.params.id, req.body)
+        .then(() => res.redirect(`/snippets/${req.params.id}`))
+        .catch((err) => console.log(err));
     },
+  ],
 
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    details: async (req, res) => {
-        res.render('snippets/details', {
-            appTitle: appTitle,
-            title: "Details",
-            snippet: await snippetRepository
-                .getSnippetByIdWithLanguageAndProjectsAndTags(req.params.id)
-        })
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  create: async (req, res) => {
+    res.render("snippets/create", {
+      appTitle: appTitle,
+      title: "Ajouter un snippet",
+      languages: await languageRepository.getAllLanguages(),
+      tags: await tagRepository.getAllTags(),
+      projects: await projectRepository.getAllProjects(),
+    });
+  },
+
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  store: [
+    validateSnippet,
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).render("snippets/create", {
+          appTitle: appTitle,
+          title: "Ajouter un snippet",
+          languages: await languageRepository.getAllLanguages(),
+          tags: await tagRepository.getAllTags(),
+          projects: await projectRepository.getAllProjects(),
+          errors: errors.array(),
+        });
+      }
+      snippetRepository
+        .insertSnippet(req.body)
+        .then(() => res.redirect("/snippets"))
+        .catch((err) => {
+          console.log(err);
+          rs;
+        });
     },
+  ],
 
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    edit: async (req, res) => {
-        res.render('snippets/edit', {
-            appTitle: appTitle,
-            title: "Edit",
-            snippet: await snippetRepository
-                .getSnippetByIdWithLanguageAndProjectsAndTags(req.params.id),
-            languages: await languageRepository.getAllLanguages(),
-            tags: await tagRepository.getAllTags(),
-            projects: await projectRepository.getAllProjects()
-        })
-    },
-
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    update: [
-        validateSnippet,
-        async (req, res) => {
-            const errors = validationResult(req)
-            if (!errors.isEmpty()) {
-                return res.status(400).render('snippets/edit', {
-                    appTitle: appTitle,
-                    title: "Edit",
-                    snippet: await snippetRepository
-                        .getSnippetByIdWithLanguageAndProjectsAndTags(req.params.id),
-                    languages: await languageRepository.getAllLanguages(),
-                    tags: await tagRepository.getAllTags(),
-                    projects: await projectRepository.getAllProjects(),
-                    errors: errors.array()
-                })
-            }
-            snippetRepository.updateSnippetById(req.params.id, req.body)
-                .then(() => res.redirect(`/snippets/${req.params.id}`))
-                .catch(err => console.log(err))
-        }
-    ],
-
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    create: async (req, res) => {
-        res.render('snippets/create', {
-            appTitle: appTitle,
-            title: "Ajouter un snippet",
-            languages: await languageRepository.getAllLanguages(),
-            tags: await tagRepository.getAllTags(),
-            projects: await projectRepository.getAllProjects()
-        })
-    },
-
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    store: [
-        validateSnippet,
-        async (req, res) => {
-            const errors = validationResult(req)
-            if (!errors.isEmpty()) {
-                return res.status(400).render(
-                    'snippets/create',
-                    {
-                        appTitle: appTitle,
-                        title: "Ajouter un snippet",
-                        languages: await languageRepository.getAllLanguages(),
-                        tags: await tagRepository.getAllTags(),
-                        projects: await projectRepository.getAllProjects(),
-                        errors: errors.array()
-                    }
-                )
-            }
-            snippetRepository.insertSnippet(req.body)
-                .then(() => res.redirect('/snippets'))
-                .catch(err => {
-                    console.log(err); rs
-                })
-        }],
-
-    /**
-     * 
-     * @param {*} req 
-     * @param {*} res 
-     */
-    delete: async (req, res) => {
-        await snippetRepository.deleteSnippetById(req.params.id)
-        res.redirect('/snippets')
-    }
-}
+  /**
+   *
+   * @param {*} req
+   * @param {*} res
+   */
+  delete: async (req, res) => {
+    await snippetRepository.deleteSnippetById(req.params.id);
+    res.redirect("/snippets");
+  },
+};
