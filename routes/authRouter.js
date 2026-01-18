c79d43b365f8357c22a34d02750078c74c425cd4
Your Name <you@example.com>
2026-01-18 15:29:13 +0100

    Update login form

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,16 +2,18 @@ import { Router } from "express";
 import passport from "passport";
 import authController from "../controllers/authController.js";
 
-const authRouter = Router()
+const authRouter = Router();
 
-authRouter.get('/login', authController.authGet)
-authRouter.post('/login', passport.authenticate("local", {
-    successRedirect: '/snippets',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
-authRouter.get('/register', authController.registerGet)
-authRouter.post('/register', authController.registerPost)
-authRouter.get('/logout', authController.logout)
+authRouter.get("/login", authController.authGet);
+authRouter.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/snippets",
+    failureRedirect: "/login",
+  }),
+);
+//authRouter.get('/register', authController.registerGet)
+//authRouter.post('/register', authController.registerPost)
+authRouter.get("/logout", authController.logout);
 
-export default authRouter
+export default authRouter;
